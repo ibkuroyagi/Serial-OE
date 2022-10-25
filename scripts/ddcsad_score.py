fix average SE

scripts/ddcsad_score.py
@@ -108,9 +108,9 @@ for i in range(12):
     se = seed_hauc_mauc_list.std(0)[i] / np.sqrt(len(seed_list))
     print(f"{ave:.2f}\pm{se:.2f}", end=", ")
 ave = seed_hauc_mauc_list.mean(0)[::2].mean()
-se = seed_hauc_mauc_list[:, ::2].std() / np.sqrt(len(seed_list) * len(machine))
+se = seed_hauc_mauc_list[:, ::2].mean(1).std() / np.sqrt(len(seed_list))
 print(f"{ave:.2f}\pm{se:.2f}", end=", ")
 ave = seed_hauc_mauc_list.mean(0)[1::2].mean()
-se = seed_hauc_mauc_list[:, 1::2].std() / np.sqrt(len(seed_list) * len(machine))
+se = seed_hauc_mauc_list[:, 1::2].mean(1).std() / np.sqrt(len(seed_list))
 print(f"{ave:.2f}\pm{se:.2f}")
 # %%

scripts/eda.py
@@ -168,10 +168,10 @@ for i in range(12):
     se = seed_hauc_mauc_list.std(0)[i] / np.sqrt(len(seed_list))
     print(f"{ave:.2f}\pm{se:.2f}", end=", ")
 ave = seed_hauc_mauc_list.mean(0)[::2].mean()
-se = seed_hauc_mauc_list[:, ::2].std() / np.sqrt(len(seed_list) * len(machine))
+se = seed_hauc_mauc_list[:, ::2].mean(1).std() / np.sqrt(len(seed_list))
 print(f"{ave:.2f}\pm{se:.2f}", end=", ")
 ave = seed_hauc_mauc_list.mean(0)[1::2].mean()
-se = seed_hauc_mauc_list[:, 1::2].std() / np.sqrt(len(seed_list) * len(machine))
+se = seed_hauc_mauc_list[:, 1::2].mean(1).std() / np.sqrt(len(seed_list))
 print(f"{ave:.2f}\pm{se:.2f}")
 
 # %%
@@ -182,7 +182,7 @@ hp_list = [2, 32, 1, 32, 1]
 seed_auc_list = np.zeros((len(seed_list), len(machines) * 2))
 seed_hauc_mauc_list = np.zeros((len(seed_list), len(machines) * 2))
 
-no = "008"
+no = "020"
 for hp_idx, h in enumerate(h_list):
     for seed in seed_list:
         score_path = f"exp/all/audioset_v{no}_0.15_seed{seed}/checkpoint-100epochs/score_embed.csv"
@@ -229,10 +229,10 @@ for hp_idx, h in enumerate(h_list):
         se = seed_hauc_mauc_list.std(0)[i] / np.sqrt(len(seed_list))
         print(f"{ave:.2f}\pm{se:.2f}", end=", ")
     ave = seed_hauc_mauc_list.mean(0)[::2].mean()
-    se = seed_hauc_mauc_list[:, ::2].std() / np.sqrt(len(seed_list) * len(machine))
+    se = seed_hauc_mauc_list[:, ::2].mean(1).std() / np.sqrt(len(seed_list))
     print(f"{ave:.2f}\pm{se:.2f}", end=", ")
     ave = seed_hauc_mauc_list.mean(0)[1::2].mean()
-    se = seed_hauc_mauc_list[:, 1::2].std() / np.sqrt(len(seed_list) * len(machine))
+    se = seed_hauc_mauc_list[:, 1::2].mean(1).std() / np.sqrt(len(seed_list))
     print(f"{ave:.2f}\pm{se:.2f}")
 # %%
 machines = ["fan", "pump", "slider", "valve", "ToyCar", "ToyConveyor"]
@@ -271,23 +271,23 @@ for no in no_list:
             ]
         seed_auc_list[seed] = auc_list
         seed_hauc_mauc_list[seed] = hauc_mauc_list
-    print(f"\n{no} hauc_mauc mean")
-    for i in range(12):
-        print(seed_hauc_mauc_list.mean(0)[i], end=", ")
-    print(f"\n{no} hauc_mauc SE")
-    for i in range(12):
-        print(seed_hauc_mauc_list.std(0)[i] / np.sqrt(len(seed_list)), end=", ")
-    # print(f"\n{no} hauc_mauc")
+    # print(f"\n{no} hauc_mauc mean")
     # for i in range(12):
-    #     ave = seed_hauc_mauc_list.mean(0)[i]
-    #     se = seed_hauc_mauc_list.std(0)[i] / np.sqrt(len(seed_list))
-    #     print(f"{ave:.2f}\pm{se:.2f}", end=", ")
-    # ave = seed_hauc_mauc_list.mean(0)[::2].mean()
-    # se = seed_hauc_mauc_list[:,::2].std() / np.sqrt(len(seed_list)*len(machine))
-    # print(f"{ave:.2f}\pm{se:.2f}", end=", ")
-    # ave = seed_hauc_mauc_list.mean(0)[1::2].mean()
-    # se = seed_hauc_mauc_list[:,1::2].std() / np.sqrt(len(seed_list)*len(machine))
-    # print(f"{ave:.2f}\pm{se:.2f}")
+    #     print(seed_hauc_mauc_list.mean(0)[i], end=", ")
+    # print(f"\n{no} hauc_mauc SE")
+    # for i in range(12):
+    #     print(seed_hauc_mauc_list.std(0)[i] / np.sqrt(len(seed_list)), end=", ")
+    print(f"\n{no} hauc_mauc")
+    for i in range(12):
+        ave = seed_hauc_mauc_list.mean(0)[i]
+        se = seed_hauc_mauc_list.std(0)[i] / np.sqrt(len(seed_list))
+        print(f"{ave:.2f}\pm{se:.2f}", end=", ")
+    ave = seed_hauc_mauc_list.mean(0)[::2].mean()
+    se = seed_hauc_mauc_list[:, ::2].mean(1).std() / np.sqrt(len(seed_list))
+    print(f"{ave:.2f}\pm{se:.2f}", end=", ")
+    ave = seed_hauc_mauc_list.mean(0)[1::2].mean()
+    se = seed_hauc_mauc_list[:, 1::2].mean(1).std() / np.sqrt(len(seed_list))
+    print(f"{ave:.2f}\pm{se:.2f}")
 # %%
 no = "020"
 machine = "fan"

scripts/plot_scores_for_paper.py
@@ -10,6 +10,7 @@ from statistics import mean
 
 machines = ["fan", "pump", "slider", "valve", "ToyCar", "ToyConveyor"]
 method_list = ["Serial-OE", "Serial Method", r"w/o $L_{\rm product}$"]
+n_normal = 1000
 
 # %%
 # DDCSAD のスコアと一緒にプロット
@@ -24,7 +25,6 @@ elif ex_name == "contaminate":
 
 seed_list = [0, 1, 2, 3, 4]
 n_anomaly_list = [0, 1, 2, 4, 8, 16, 32]
-n_normal = 1000
 # anomaly_percent = [f"{per/(n_normal)*100:.1f}" for per in n_anomaly_list]
 anomaly_percent = [per / (n_normal) * 100 for per in n_anomaly_list]
 
@@ -69,53 +69,6 @@ for l, no in enumerate(no_list):
                     * 100
                 )
 use_one_fig = True
-# for i, machine in enumerate(machines):
-#     title_no = ""
-#     if use_one_fig:
-#         fig, ax = plt.subplots()
-#     for l, no in enumerate(no_list):
-#         title_no += f"{no}_"
-#         if not use_one_fig:
-#             fig, ax = plt.subplots()
-#         yerr_se = anomaly_score_list[l, i].std(1) / np.sqrt(len(seed_list))
-#         ddcsad_se = ddcsad_score_list[i].std(1) / np.sqrt(len(seed_list))
-#         x = np.arange(len(n_anomaly_list))
-#         y_mean = anomaly_score_list[l, i].mean(1)
-#         ddcsad_mean = ddcsad_score_list[i].mean(1)
-#         ax.plot(x, y_mean, marker="o", label=f"{no} {machine}")
-#         ax.errorbar(
-#             x,
-#             y_mean,
-#             yerr=yerr_se,
-#             capsize=3,
-#             fmt="o",
-#             ecolor="k",
-#             ms=7,
-#             mfc="None",
-#             mec="k",
-#         )
-#     ax.plot(x, ddcsad_mean, marker="o", label=f"DDCSAD {machine}")
-#     ax.errorbar(
-#         x,
-#         ddcsad_mean,
-#         yerr=ddcsad_se,
-#         capsize=3,
-#         fmt="o",
-#         ecolor="k",
-#         ms=7,
-#         mfc="None",
-#         mec="k",
-#     )
-#     if use_one_fig:
-#         ax.set_xticklabels([0] + n_anomaly_list)
-#         ax.set_ylim()
-#         ax.set_xlabel("The number of anomaly samples")
-#         ax.set_ylabel("ave-AUC [%]")
-#         ax.set_title(f"The number of anomalous data vs. {machine} ASD performance")
-#         os.makedirs("exp/fig", exist_ok=True)
-#         plt.legend()
-#         plt.tight_layout()
-#         plt.savefig(f"exp/fig/{title_no}w_ddcsad_{machine}.png")
 title_no = ""
 if use_one_fig:
     fig, ax = plt.subplots(figsize=(8, 7))
@@ -159,7 +112,7 @@ plt.savefig(f"exp/fig/paper_{ex_name}_aAUC_all.png")
 # mAUCの性能をプロット
 # 異常スコアの変遷をエラーバー(標準誤差,SE)をプロットする
 ex_name = "use"
-ex_name = "contaminate"
+# ex_name = "contaminate"
 
 if ex_name == "use":
     no_list = ["120", "108", "129"]
@@ -202,54 +155,6 @@ for l, no in enumerate(no_list):
                     ddcsad_df.loc[0, [f"dev_{machine}_mauc"]] * 100
                 )
 use_one_fig = True
-
-# for i, machine in enumerate(machines):
-#     title_no = ""
-#     if use_one_fig:
-#         fig, ax = plt.subplots()
-#     for l, no in enumerate(no_list):
-#         title_no += f"{no}_"
-#         if not use_one_fig:
-#             fig, ax = plt.subplots()
-#         yerr_se = anomaly_score_list[l, i].std(1) / np.sqrt(len(seed_list))
-#         ddcsad_se = ddcsad_score_list[i].std(1) / np.sqrt(len(seed_list))
-#         x = np.arange(len(n_anomaly_list))
-#         y_mean = anomaly_score_list[l, i].mean(1)
-#         ddcsad_mean = ddcsad_score_list[i].mean(1)
-#         ax.plot(x, y_mean, marker="o", label=f"{no} {machine}")
-#         ax.errorbar(
-#             x,
-#             y_mean,
-#             yerr=yerr_se,
-#             capsize=3,
-#             fmt="o",
-#             ecolor="k",
-#             ms=7,
-#             mfc="None",
-#             mec="k",
-#         )
-#     ax.plot(x, ddcsad_mean, marker="o", label=f"DDCSAD {machine}")
-#     ax.errorbar(
-#         x,
-#         ddcsad_mean,
-#         yerr=ddcsad_se,
-#         capsize=3,
-#         fmt="o",
-#         ecolor="k",
-#         ms=7,
-#         mfc="None",
-#         mec="k",
-#     )
-#     if use_one_fig:
-#         ax.set_xticklabels([0] + n_anomaly_list)
-#         ax.set_ylim()
-#         ax.set_xlabel("The number of anomaly samples")
-#         ax.set_ylabel("ave-AUC [%]")
-#         ax.set_title(f"The number of anomalous data vs. {machine} mAUC")
-#         os.makedirs("exp/fig", exist_ok=True)
-#         plt.legend()
-#         plt.tight_layout()
-#         plt.savefig(f"exp/fig/{title_no}w_ddcsad_{machine}_mAUC.png")
 if use_one_fig:
     fig, ax = plt.subplots(figsize=(8, 7))
 for l, no in enumerate(method_list):
@@ -283,7 +188,7 @@ ax.errorbar(
     mec="k",
 )
 ax.set_xlabel("The ratio of anomalous data to normal data [%]", fontsize=16)
-ax.set_ylabel("The average AUC [%]", fontsize=16)
+ax.set_ylabel("The minimum AUC [%]", fontsize=16)
 ax.grid()
 plt.legend(fontsize=16)
 plt.tight_layout()