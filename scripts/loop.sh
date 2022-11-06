9e941cf7fc755953a0c68a1b88db7b51520afdc4
ibkuroyagi <ibkuroyagi@gmail.com>
2022-11-06 09:52:19 +0900

    fix

--- scripts/loop.sh
+++ scripts/loop.sh
@@ -5,12 +5,13 @@
 # training stage
 for method in only_l_product only_l_machine serial_oe serial_method wo_l_machine wo_l_product wo_mixup; do
     ./job.sh --model_name "${method}.normal"
-    ./job.sh --model_name "${method}.utilize" --n_anomaly 16
-    ./job.sh --model_name "${method}.contaminated" --n_anomaly 16
+    ./job.sh --model_name "${method}.utilize" --n_anomaly 32
+    ./job.sh --model_name "${method}.contaminated" --n_anomaly 32
 done
+sleep 10000
 # scoring stage
 for method in only_l_product only_l_machine serial_oe serial_method wo_l_machine wo_l_product wo_mixup; do
     ./job.sh --model_name "${method}.normal" --stage 2
-    ./job.sh --model_name "${method}.utilize" --n_anomaly 16 --stage 2
-    ./job.sh --model_name "${method}.contaminated" --n_anomaly 16 --stage 2
+    ./job.sh --model_name "${method}.utilize" --n_anomaly 32 --stage 2
+    ./job.sh --model_name "${method}.contaminated" --n_anomaly 32 --stage 2
 done
