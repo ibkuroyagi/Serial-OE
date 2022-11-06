c9ad4dbb04e27f69991bee42191a5afc5f25e601
ibkuroyagi <ibkuroyagi@gmail.com>
2022-11-06 09:55:05 +0900

    fix loop

--- scripts/loop.sh
+++ scripts/loop.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-# shellcheck disable=SC1091
-. utils/original_funcs.sh || exit 1
 # training stage
 for method in only_l_product only_l_machine serial_oe serial_method wo_l_machine wo_l_product wo_mixup; do
     ./job.sh --model_name "${method}.normal"
     ./job.sh --model_name "${method}.utilize" --n_anomaly 32
     ./job.sh --model_name "${method}.contaminated" --n_anomaly 32
 done
-sleep 10000
+
 # scoring stage
 for method in only_l_product only_l_machine serial_oe serial_method wo_l_machine wo_l_product wo_mixup; do
     ./job.sh --model_name "${method}.normal" --stage 2
